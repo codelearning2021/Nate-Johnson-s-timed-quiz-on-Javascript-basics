26405e186d9c122f80a31d49a0100367b225d3b0
codelearning2021 <82570118+codelearning2021@users.noreply.github.com>
2023-01-09 04:25:50 -0800

    Updated code with better working examples, adapted a timer that will work with this framework

--- script.js
+++ script.js
@@ -1,126 +1,157 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "What is capital of India?",
+    a: [{ text: "gandhinagar", isCorrect: false },
+    { text: "Surat", isCorrect: false },
+    { text: "Delhi", isCorrect: true },
+    { text: "mumbai", isCorrect: false }
+    ]
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+},
+{
+    id: 1,
+    q: "What is the capital of Thailand?",
+    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+    { text: "phuket", isCorrect: false },
+    { text: "Ayutthaya", isCorrect: false },
+    { text: "Bangkok", isCorrect: true }
+    ]
 
-    function showQuestions(questions, quizContainer) {
-        // code will go here
-    }
+},
+{
+    id: 2,
+    q: "What is the capital of Gujarat",
+    a: [{ text: "surat", isCorrect: false },
+    { text: "vadodara", isCorrect: false },
+    { text: "gandhinagar", isCorrect: true },
+    { text: "rajkot", isCorrect: false }
+    ]
 
-    function showResults(questions, quizContainer, resultsContainer) {
-        // code will go here
-    }
-
-    // show the questions
-    showQuestions(questions, quizContainer);
-
-    // when user clicks submit, show results
-    submitButton.onclick = function () {
-        showResults(questions, quizContainer, resultsContainer);
-    }
 }
 
+]
 
-var myQuestions = [
-    {
-        question: "What is 10/2?",
-        answers: {
-            a: '3',
-            b: '5',
-            c: '115'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "What is 30/3?",
-        answers: {
-            a: '3',
-            b: '5',
-            c: '10'
-        },
-        correctAnswer: 'c'
-    }
-];
+// Set start
+var start = true;
 
-function showQuestions(questions, quizContainer) {
-    // we'll need a place to store the output and the answer choices
-    var output = [];
-    var answers;
+// Iterate
+function iterate(id) {
 
-    // for each question...
-    for (var i = 0; i < questions.length; i++) {
+    // Getting the result display section
+    var result = document.getElementsByClassName("result");
+    result[0].innerText = "";
 
-        // first reset the list of answers
-        answers = [];
+    // Getting the question
+    const question = document.getElementById("question");
 
-        // for each available answer to this question...
-        for (letter in questions[i].answers) {
 
-            // ...add an html radio button
-            answers.push(
-                '<label>'
-                + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                + letter + ': '
-                + questions[i].answers[letter]
-                + '</label>'
-            );
+    // Setting the question text
+    question.innerText = Questions[id].q;
+
+    // Getting the options
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
+
+
+    // Providing option text
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
+
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
+
+    var selected = "";
+
+    // Show selection for op1
+    op1.addEventListener("click", () => {
+        op1.style.backgroundColor = "lightgoldenrodyellow";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op1.value;
+    })
+
+    // Show selection for op2
+    op2.addEventListener("click", () => {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightgoldenrodyellow";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op2.value;
+    })
+
+    // Show selection for op3
+    op3.addEventListener("click", () => {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightgoldenrodyellow";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op3.value;
+    })
+
+    // Show selection for op4
+    op4.addEventListener("click", () => {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightgoldenrodyellow";
+        selected = op4.value;
+    })
+
+    // Grabbing the evaluate button
+    const evaluate = document.getElementsByClassName("evaluate");
+
+    // Evaluate method
+    evaluate[0].addEventListener("click", () => {
+        if (selected == "true") {
+            result[0].innerHTML = "True";
+            result[0].style.color = "green";
+        } else {
+            result[0].innerHTML = "False";
+            result[0].style.color = "red";
         }
+    })
+}
 
-        // add this question and its answers to the output
-        output.push(
-            '<div class="question">' + questions[i].question + '</div>'
-            + '<div class="answers">' + answers.join('') + '</div>'
-        );
+if (start) {
+    iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+    start = false;
+    if (id < 2) {
+        id++;
+        iterate(id);
+        console.log(id);
     }
 
-    // finally combine our output list into one string of html and put it on the page
-    quizContainer.innerHTML = output.join('');
-}
+})
 
-showQuestions(questions, quizContainer);
+// set up timer for quiz
 
+var sec = 60;
+var time = setInterval(myTimer, 1000);
 
-function showResults(questions, quizContainer, resultsContainer) {
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + " seconds left";
+    sec--;
+    if (sec < 0) {
+        clearInterval(time);
+        var initials = prompt("Time out!! Type your initials");
+        document.getElementById("initialScore").innerHTML =
+            "Initials: " + initials + " Score: " + ;
 
-    // gather answer containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll('.answers');
-
-    // keep track of user's answers
-    var userAnswer = '';
-    var numCorrect = 0;
-
-    // for each question...
-    for (var i = 0; i < questions.length; i++) {
-
-        // find selected answer
-        userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-        // if answer is correct
-        if (userAnswer === questions[i].correctAnswer) {
-            // add to the number of correct answers
-            numCorrect++;
-
-            // color the answers green
-            answerContainers[i].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else {
-            // color the answers red
-            answerContainers[i].style.color = 'red';
-        }
     }
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
 }
-
-
-
-// on submit, show results
-submitButton.onclick = function () {
-    showResults(questions, quizContainer, resultsContainer);
-}
-
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
