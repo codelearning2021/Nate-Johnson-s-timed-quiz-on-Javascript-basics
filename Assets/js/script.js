d0edd03e2d64154cfd1bfe8d01df8cde0817220a
codelearning2021 <82570118+codelearning2021@users.noreply.github.com>
2023-01-22 04:45:01 -0800

    fixed the start button, adding logic to end quiz but will need to debug current logic

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,15 +3,13 @@ var startContainer = document.querySelector(".start-container")
 var startBtn = document.querySelector(".start-btn")
 var panel = document.querySelector(".panel")
 
+// start button kicking off quiz
 startBtn.addEventListener("click", function () {
     startContainer.classList.add("hide");
     panel.classList.remove("hide");
     var time = setInterval(() => myTimer(time), 1000);
-    if {
-        question.innerText == Questions[id].2;
-
-    }
-})
+}
+)
 
 // Questions will be asked
 const Questions = [{
@@ -36,15 +34,13 @@ const Questions = [{
 },
 {
     id: 2,
-    q: "What html element do you need to add a Javascript file",
+    q: "What html element do you need to add a Javascript file?",
     a: [{ text: "<style src=>", isCorrect: false },
     { text: "<div class=>", isCorrect: false },
     { text: "<script src=>", isCorrect: true },
     { text: "<span>", isCorrect: false }
     ]
-
 }
-
 ]
 
 // Set score variable
@@ -67,97 +63,107 @@ function iterate(id) {
     // Setting the question text
     question.innerText = Questions[id].q;
 
-    // Getting the options
-    const op1 = document.getElementById('op1');
-    const op2 = document.getElementById('op2');
-    const op3 = document.getElementById('op3');
-    const op4 = document.getElementById('op4');
+    // Changing the Next button text
+
+    function endQuiz() {
+        if (question.innerText = "What html element do you need to add a Javascript file?") {
+            document.getElementByClassName("next").innerHTML = "End Quiz";
+        };
+
+        // Getting the options
+        const op1 = document.getElementById('op1');
+        const op2 = document.getElementById('op2');
+        const op3 = document.getElementById('op3');
+        const op4 = document.getElementById('op4');
 
 
-    // Providing option text
-    op1.innerText = Questions[id].a[0].text;
-    op2.innerText = Questions[id].a[1].text;
-    op3.innerText = Questions[id].a[2].text;
-    op4.innerText = Questions[id].a[3].text;
+        // Providing option text
+        op1.innerText = Questions[id].a[0].text;
+        op2.innerText = Questions[id].a[1].text;
+        op3.innerText = Questions[id].a[2].text;
+        op4.innerText = Questions[id].a[3].text;
 
-    // Providing the true or false value to the options
-    op1.value = Questions[id].a[0].isCorrect;
-    op2.value = Questions[id].a[1].isCorrect;
-    op3.value = Questions[id].a[2].isCorrect;
-    op4.value = Questions[id].a[3].isCorrect;
+        // Providing the true or false value to the options
+        op1.value = Questions[id].a[0].isCorrect;
+        op2.value = Questions[id].a[1].isCorrect;
+        op3.value = Questions[id].a[2].isCorrect;
+        op4.value = Questions[id].a[3].isCorrect;
 
-    var selected = "";
+        var selected = "";
 
-    // Show selection for op1
-    op1.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightgoldenrodyellow";
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op4.style.backgroundColor = "lightskyblue";
-        selected = op1.value;
-    })
+        // Show selection for op1
+        op1.addEventListener("click", () => {
+            op1.style.backgroundColor = "lightgoldenrodyellow";
+            op2.style.backgroundColor = "lightskyblue";
+            op3.style.backgroundColor = "lightskyblue";
+            op4.style.backgroundColor = "lightskyblue";
+            selected = op1.value;
+        })
 
-    // Show selection for op2
-    op2.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue";
-        op2.style.backgroundColor = "lightgoldenrodyellow";
-        op3.style.backgroundColor = "lightskyblue";
-        op4.style.backgroundColor = "lightskyblue";
-        selected = op2.value;
-    })
+        // Show selection for op2
+        op2.addEventListener("click", () => {
+            op1.style.backgroundColor = "lightskyblue";
+            op2.style.backgroundColor = "lightgoldenrodyellow";
+            op3.style.backgroundColor = "lightskyblue";
+            op4.style.backgroundColor = "lightskyblue";
+            selected = op2.value;
+        })
 
-    // Show selection for op3
-    op3.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue";
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightgoldenrodyellow";
-        op4.style.backgroundColor = "lightskyblue";
-        selected = op3.value;
-    })
+        // Show selection for op3
+        op3.addEventListener("click", () => {
+            op1.style.backgroundColor = "lightskyblue";
+            op2.style.backgroundColor = "lightskyblue";
+            op3.style.backgroundColor = "lightgoldenrodyellow";
+            op4.style.backgroundColor = "lightskyblue";
+            selected = op3.value;
+        })
 
-    // Show selection for op4
-    op4.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue";
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op4.style.backgroundColor = "lightgoldenrodyellow";
-        selected = op4.value;
-    })
+        // Show selection for op4
+        op4.addEventListener("click", () => {
+            op1.style.backgroundColor = "lightskyblue";
+            op2.style.backgroundColor = "lightskyblue";
+            op3.style.backgroundColor = "lightskyblue";
+            op4.style.backgroundColor = "lightgoldenrodyellow";
+            selected = op4.value;
+        })
 
-    // Grabbing the evaluate button
-    const evaluate = document.getElementsByClassName("evaluate");
+        // Grabbing the evaluate button
+        const evaluate = document.getElementsByClassName("evaluate");
 
-    // Evaluate method
-    evaluate[0].addEventListener("click", () => {
-        if (selected == "true") {
-            result[0].innerHTML = "True";
-            result[0].style.color = "green";
-            myScore++;
-        } else {
-            result[0].innerHTML = "False";
-            result[0].style.color = "red";
-            sec -= 5;
-        }
-    })
-}
-
-if (start) {
-    iterate("0");
-}
-
-// Next button and method
-var next = document.getElementsByClassName('next')[0];
-var id = 0;
-
-next.addEventListener("click", () => {
-    start = false;
-    if (id < 2) {
-        id++;
-        iterate(id);
-        console.log(id);
+        // Evaluate method
+        evaluate[0].addEventListener("click", () => {
+            if (selected == "true") {
+                result[0].innerHTML = "True";
+                result[0].style.color = "green";
+                myScore++;
+            } else {
+                result[0].innerHTML = "False";
+                result[0].style.color = "red";
+                sec -= 5;
+            }
+        })
     }
 
-})
+    if (start) {
+        iterate("0");
+    }
+
+    // Next button and method
+    var next = document.getElementsByClassName('next')[0];
+    var id = 0;
+
+    next.addEventListener("click", () => {
+        start = false;
+        if (id < 2) {
+            id++;
+            iterate(id);
+        }
+        else if (id > 3) {
+            sec = 0;
+        }
+    });
+
+}
 
 // set up timer for quiz
 
@@ -168,16 +174,16 @@ function myTimer(time) {
     sec--;
     if (sec < 0) {
         clearInterval(time); ``
-        var initials = prompt("Time out!! Type your initials");
+        var initials = prompt("That's it!! Type your initials");
         if (initials == "") {
             document.getElementById("initialScore").innerHTML =
                 "Initials: none" + " Score: " + myScore;
-
         }
-        else {
-            document.getElementById("initialScore").innerHTML =
-                "Initials: " + initials + " Score: " + myScore;
+        else if (initials === null) {
+            "Initials: none" + " Score: " + myScore;
         }
-        localStorage.setItem(initials, myScore);
+        else document.getElementById("initialScore").innerHTML =
+            "Initials: " + initials + " Score: " + myScore;
     }
+    localStorage.setItem(initials, myScore);
 }
